0477d24dee10b919d86867bbbecb0e7f7569da9d
Rayhan Noufal Arayilakath <me@rayhanadev.com>
2026-03-12 20:04:44 -0400

    feat: kaomoji

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,10 @@
 import { Hono } from "hono";
 import { bearerAuth } from "hono/bearer-auth";
 import preferences from "./routes/preferences.ts";
-import { Mode, Project } from "./enums.ts";
-import { DEFAULT_MODE } from "./constants.ts";
 
 const app = new Hono();
 
-app.get("/", (c) =>
-  c.json({
-    name: "privacydb",
-    status: "ok",
-    modes: Object.values(Mode),
-    default_mode: DEFAULT_MODE,
-    projects: Object.values(Project),
-  }),
-);
+app.get("/", (c) => c.text("(╭ರ_•́)?"));
 
 app.use("/*", bearerAuth({ token: process.env.API_KEY! }));
 
